d92094bb7e68b2dc37035d754e747f37d80ae29f
horus <horus@HIERAKONPOLIS.mainframe>
2017-07-13 02:21:20 -0400

    Initial commit

--- dbh.php
+++ dbh.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "QWEasd123#()";
+$username = "public";
+$password = "openpass";
 $db = "login-test";
 
 $conn = mysqli_connect($servername, $username, $password, $db);
